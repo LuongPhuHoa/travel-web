26b0b0274d9dd5ddb9a99c36134c3083eb90592f
LuongPhuHoa <36793008+LuongPhuHoa@users.noreply.github.com>
2023-10-11 10:27:24 +0700

    commit
    commit

--- app/components/banner.js
+++ app/components/banner.js
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "./button";
 import "./home/style.css";
 const Banner = ({ title, description, style }) => {
-    <div className="banner" style={{style}}>
+    <div className=" bg-center bg-no-repeat" style={style}>
         <div className="flex items-center justify-center">
             <h1 className="banner-title text-center text-5xl font-bold text-white">{title}</h1>
             <h1 className="banner-description text-center text-2xl font-medium text-white">{description}</h1>
